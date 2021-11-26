877328ea2697f014a51a67c44adcb51b45eb7467
Arne Sørli <arnesor@users.noreply.github.com>
2021-11-26 07:23:47 +0100

    Write to log file

--- pytime.py
+++ pytime.py
@@ -1,105 +1,96 @@
 #!/usr/bin/env python3
 
-from datetime import datetime
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.common.exceptions import WebDriverException
 from time import sleep
+import chromedriver_autoinstaller
 import argparse
 import requests
-import statistics
 import sys
-
-url_list = ["https://www.google.no/",
-            "https://www.nrk.no/",
-            "http://www.eidskog-o-lag.no/",
-            "https://www.eidskog.kommune.no/"]
-
-filename = "webtimingslog.txt"
+import logging
 
 
-def write_to_file(url, backend_time, frontend_time):
-    now = datetime.now()
-    with open(filename, 'a') as file:
-        if backend_time is not None and frontend_time is not None:
-            file.write(f"{now}, {url}, {backend_time}, {frontend_time}\n")
-        else:
-            file.write(f"{now}, {url}, {backend_time}, {frontend_time}\n")
+logging.basicConfig(
+    handlers=[logging.FileHandler("connection.log"), logging.StreamHandler()],
+    level=logging.INFO,
+    format="%(asctime)s %(levelname)s %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
+
+url_list = [
+    "https://www.google.no/",
+    "https://www.nrk.no/",
+    "https://www.eidskog.kommune.no/",
+]
 
 
 def open_webpage(url):
-    driver = webdriver.Chrome()
+    timeout = 5  # seconds
+    chrome_options = Options()
+    chrome_options.headless = True
+    driver = webdriver.Chrome(options=chrome_options)
+    driver.set_page_load_timeout(timeout)
     driver.delete_all_cookies()
-    driver.get(url)
+    selenium_error = False
 
-    navigation_start = driver.execute_script("return window.performance.timing.navigationStart")
-    domain_start = driver.execute_script("return window.performance.timing.domainLookupStart")
-    connect_end = driver.execute_script("return window.performance.timing.connectEnd")
-    response_start = driver.execute_script("return window.performance.timing.responseStart")
-    dom_complete = driver.execute_script("return window.performance.timing.domComplete")
+    try:
+        driver.get(url)
 
-    backend_performance_calc = response_start - navigation_start
-    dns_tcp_calc = connect_end - domain_start
-    frontend_performance_calc = dom_complete - response_start
-    total_time = dom_complete - navigation_start
+        navigation_start = driver.execute_script(
+            "return window.performance.timing.navigationStart"
+        )
+        domain_start = driver.execute_script(
+            "return window.performance.timing.domainLookupStart"
+        )
+        connect_end = driver.execute_script(
+            "return window.performance.timing.connectEnd"
+        )
+        response_start = driver.execute_script(
+            "return window.performance.timing.responseStart"
+        )
+        dom_complete = driver.execute_script(
+            "return window.performance.timing.domComplete"
+        )
+
+        backend_performance_calc = response_start - navigation_start
+        dns_tcp_calc = connect_end - domain_start
+        frontend_performance_calc = dom_complete - response_start
+        total_time = dom_complete - navigation_start
+
+        log_msg = f"200 {url} {dns_tcp_calc} {backend_performance_calc} {frontend_performance_calc} "
+        logging.info(log_msg)
+    except WebDriverException as e:
+        try:
+            r = requests.get(url, timeout=timeout)
+            logging.error(f'{r.status_code} {url} "{e.msg}"')
+        except requests.exceptions.RequestException as e:
+            logging.error(f"408 {url} Timeout")
 
     driver.quit()
 
-    # Only return timing if page loaded successfully, otherwise None
-    try:
-        r = requests.get(url, timeout=10)
-    except requests.exceptions.RequestException as e:
-        print(f"Timeout loading page {url}")
-        return None, None
-
-    if r.status_code == 200:
-        print("URL       : %s" % url)
-        print("Back End  : %s" % backend_performance_calc)
-        print("DNS+TCP   : %s" % dns_tcp_calc)
-        print("Front End : %s" % frontend_performance_calc)
-        print("Total time: %s" % total_time)
-        return backend_performance_calc, frontend_performance_calc
-    else:
-        print(f"Timeout loading page {url}")
-        return None, None
-
 
 def main():
-    backend_performance_list = []
-    frontend_performance_list = []
+    chromedriver_autoinstaller.install()
 
     if args.urllist:
         for _ in range(args.iterations):
             for link in url_list:
-                result = open_webpage(link)
-                if result[0] is not None:
-                    backend_performance_list.append(result[0])
-                if result[1] is not None:
-                    frontend_performance_list.append(result[1])
-                write_to_file(link, result[0], result[1])
+                open_webpage(link)
     else:
         for _ in range(args.iterations):
-            result = open_webpage(args.url)
-            if result[0] is not None:
-                backend_performance_list.append(result[0])
-            if result[1] is not None:
-                frontend_performance_list.append(result[1])
-            write_to_file(args.url, result[0], result[1])
-
-    print('Backend       : {}'.format(backend_performance_list))
-    print('Frontend      : {}'.format(frontend_performance_list))
-    if backend_performance_list and frontend_performance_list:
-        print('Backend mean  : {}'.format(statistics.mean(backend_performance_list)))
-        print('Backend median: {}'.format(statistics.median(backend_performance_list)))
-        print('Backend min   : {}'.format(min(backend_performance_list)))
-        print('Backend max   : {}'.format(max(backend_performance_list)))
+            open_webpage(args.url)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     parser = argparse.ArgumentParser(description=sys.modules[__name__].__doc__)
-    parser.add_argument('-u', '--urllist', action='store_true', help="Use predefined list of urls")
-    parser.add_argument('url', nargs='?', default=url_list[0])
-    parser.add_argument('iterations', nargs='?', type=int, default=1)
+    parser.add_argument(
+        "-u", "--urllist", action="store_true", help="Use predefined list of urls"
+    )
+    parser.add_argument("url", nargs="?", default=url_list[0])
+    parser.add_argument("iterations", nargs="?", type=int, default=1)
     args = parser.parse_args()
 
     while True:
         main()
-        sleep(15*60)  # Run each 15 minutes
+        sleep(10*60)  # Run each 10 minutes
